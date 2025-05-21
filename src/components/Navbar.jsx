b1caef43de88c6e5d726efd983ff6955878f0dab
David Potter <davidgeorgepotter@protonmail.com>
2025-05-21 20:27:37 +0100

    Add logged in user status to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
+import { useContext } from "react";
+import { LoggedInUserContext } from "../contexts/LoggedInUser";
 
 export const Navbar = () => {
+  const { loggedInUser } = useContext(LoggedInUserContext);
   return (
     <nav>
       <Link to="/" className="navlinks">
@@ -9,6 +12,11 @@ export const Navbar = () => {
       <Link to="/User" className="navlinks">
         User Account
       </Link>
+      {loggedInUser ? (
+        <p className="loggedInNavMessage">
+          Logged in as {loggedInUser.username}
+        </p>
+      ) : null}
     </nav>
   );
 };
